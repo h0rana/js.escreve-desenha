b6e98cdd8adbd0423390cc72910a4b1464456143
al.12 <00001098827806sp@al.educacao.sp.gov.br>
2024-05-23 00:01:16 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("blue")
+  }
+  
+  function draw() {
+    stroke("white");
+    fill("purple");
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35)
+    }
+  
+  }
